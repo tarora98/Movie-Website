0cf151035d46eb25fd3523f22b2c1b34b64fd8cf
tarora06 <59690247+tarora06@users.noreply.github.com>
2020-08-07 21:50:42 +0530

    Web

---
+++ src/modules/movie-browser/movie-browser.service.js
@@ -0,0 +1,33 @@
+// WARNING: Don't check your actual API key into GitHub
+import { MOVIE_DB_API_KEY } from '../../api-keys';
+const MOVIE_DB_BASE_URL = 'https://api.themoviedb.org/3';
+const createMovieDbUrl = (relativeUrl, queryParams) => {
+  let baseUrl = `${MOVIE_DB_BASE_URL}${relativeUrl}?api_key=${MOVIE_DB_API_KEY}&language=en-US`;
+  if (queryParams) {
+    Object.keys(queryParams)
+      .forEach(paramName => baseUrl += `&${paramName}=${queryParams[paramName]}`);
+  }
+  return baseUrl;
+}
+
+// WARNING: Must pass parameters like this: ({param1, param2})
+// for async action creator helper to work
+export const getTopMovies = async ({ page }) => {
+  const fullUrl = createMovieDbUrl('/movie/top_rated', {
+    page
+  });
+  return fetch(fullUrl);
+}
+
+export const searchMovies = async ({ page, query }) => {
+  const fullUrl = createMovieDbUrl('/search/movie', {
+    page,
+    query
+  });
+  return fetch(fullUrl);
+}
+
+export const getMovieDetails = async ({ movieId }) => {
+  const fullUrl = createMovieDbUrl(`/movie/${movieId}`);
+  return fetch(fullUrl);
+}
